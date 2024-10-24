d922b9d5bbf49064f2a83b20d33d86dd26e4bfa2
PRIYA REETHIKA LANKA <priyareethikalanka@gmail.com>
2024-10-24 19:05:35 +0530

    sign-up
    changes needed

--- Ninico-nextjs-v1.0.0-unzip-first/1.Ninico-Nextjs/app/sign-in/page.js
+++ Ninico-nextjs-v1.0.0-unzip-first/1.Ninico-Nextjs/app/sign-in/page.js
@@ -56,7 +56,7 @@ export default function SignIn() {
                             <div className="col-lg-6 col-sm-12">
                                 <div className="tptrack__product mb-40">
                                     <div className="tptrack__thumb">
-                                        <img src="signin-image.jpg" alt="" />
+                                        <img src="sign-up-image.jpg" alt="" />
                                     </div>
                                     <div className="tptrack__content grey-bg-3">
                                         <div className="tptrack__item d-flex mb-20">
